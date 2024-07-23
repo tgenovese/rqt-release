14a91ce7e0d7ded2d088292072574fe7867b9a90
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-23 12:17:16 +0200

    Imported upstream version '1.7.2' of 'upstream'

--- rqt_gui_cpp/src/rqt_gui_cpp/roscpp_plugin_provider.hpp
+++ rqt_gui_cpp/src/rqt_gui_cpp/roscpp_plugin_provider.hpp
@@ -30,72 +30,37 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef rqt_gui_cpp__NodeletPluginProvider_H
-#define rqt_gui_cpp__NodeletPluginProvider_H
+#ifndef RQT_GUI_CPP__ROSCPP_PLUGIN_PROVIDER_HPP_
+#define RQT_GUI_CPP__ROSCPP_PLUGIN_PROVIDER_HPP_
 
-#include <qt_gui_cpp/ros_pluginlib_plugin_provider.hpp>
-
-#include <rqt_gui_cpp/plugin.h>
-
-#include <rclcpp/rclcpp.hpp>
-
+#include <QMessageBox>
 #include <QThread>
 
 #include <string>
-#include <unistd.h>
-#include <iostream>
 
-namespace rqt_gui_cpp {
+#include <qt_gui_cpp/composite_plugin_provider.hpp>
 
-class RosCppPluginProvider;
-
-class NodeletPluginProvider
-  : public qt_gui_cpp::RosPluginlibPluginProvider<rqt_gui_cpp::Plugin>
+namespace rqt_gui_cpp
 {
 
+class RosCppPluginProvider
+  : public qt_gui_cpp::CompositePluginProvider
+{
 public:
+  RosCppPluginProvider();
 
-  NodeletPluginProvider(const QString& export_tag, const QString& base_class_type);
+  virtual ~RosCppPluginProvider();
 
-  virtual ~NodeletPluginProvider();
+  virtual void * load(const QString & plugin_id, qt_gui_cpp::PluginContext * plugin_context);
 
-  virtual void unload(void* instance);
+  virtual qt_gui_cpp::Plugin * load_plugin(
+    const QString & plugin_id,
+    qt_gui_cpp::PluginContext * plugin_context);
 
 protected:
+  void init_rclcpp();
 
-  void init_loader();
-
-  virtual std::shared_ptr<Plugin> create_plugin(const std::string& lookup_name, qt_gui_cpp::PluginContext* plugin_context);
-
-  virtual void init_plugin(const QString& plugin_id, qt_gui_cpp::PluginContext* plugin_context, qt_gui_cpp::Plugin* plugin);
-
-  std::shared_ptr<rqt_gui_cpp::Plugin> instance_;
-
-  QMap<void*, QString> instances_;
-
-  bool loader_initialized_;
-
-  // A shared node that is copied into each rqt_gui_cpp::PluginContext for use by rqt nodes
-  std::shared_ptr<rclcpp::Node> node_;
-
-
-  class RosSpinThread
-    : public QThread
-  {
-  public:
-    RosSpinThread(QObject* parent = 0);
-    virtual ~RosSpinThread();
-    void run();
-    bool abort;
-    // Create an executor that will be responsible for execution of callbacks for a set of nodes.
-    // With this version, all callbacks will be called from within this thread (the main one).
-    rclcpp::executors::MultiThreadedExecutor exec_;
-  };
-
-  RosSpinThread* ros_spin_thread_;
-
+  bool rclcpp_initialized_;
 };
-
-}
-
-#endif // rqt_gui_cpp__NodeletPluginProvider_H
+}  // namespace rqt_gui_cpp
+#endif  // RQT_GUI_CPP__ROSCPP_PLUGIN_PROVIDER_HPP_
